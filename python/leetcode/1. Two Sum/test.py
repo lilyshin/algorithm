527b3335dbfb731091df3aeb097af3697aa4e9b8
lilyshin <lilyshin.dev@gmail.com>
2025-07-22 05:50:35 +0900

    add: 1.two sum solution, test

---
+++ python/leetcode/1. Two Sum/test.py
@@ -0,0 +1,8 @@
+# 1. Two Sum 테스트 코드 예시
+from solution import twoSum
+
+if __name__ == "__main__":
+    # 예시 테스트 케이스
+    print(twoSum([2, 7, 11, 15], 9))      # [0, 1]
+    print(twoSum([3, 2, 4], 6))           # [1, 2]
+    print(twoSum([3, 3], 6))              # [0, 1] 
