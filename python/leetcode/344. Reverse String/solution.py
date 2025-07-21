5d28b128bcd6301c5d3a65c3729f72af3141f4f7
lilyshin <lilyshin.dev@gmail.com>
2025-07-22 05:53:12 +0900

    docs: 344 problem, description

---
+++ python/leetcode/344. Reverse String/solution.py
@@ -0,0 +1,17 @@
+# Write a function that reverses a string. The input string is given as an array of characters s.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+# 문자열을 뒤집는 함수를 작성하세요. 입력 문자열은 문자(char)들의 배열 s로 주어집니다.
+# 반드시 입력 배열을 직접 수정하여 문자열을 뒤집어야 하며, 추가적인 메모리는 O(1)만 사용해야 합니다.
+
+# Example 1:
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+
+# Example 2:
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+ 
+
+# Constraints:
+# 1 <= s.length <= 105
+# s[i] is a printable ascii character.
